57f957bc3f5c67fa12c4a0bc828ecbd02bf211f8
luizebvalente <luizebvalente@gmail.com>
2025-06-22 12:35:34 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 
 // Context
