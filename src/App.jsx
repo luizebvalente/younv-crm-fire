c7bd5d789e709b38c5892b1da025d6ba42ebaa1b
luizebvalente <luizebvalente@gmail.com>
2025-06-22 12:29:28 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -20,21 +20,20 @@ import Login from './components/pages/Login'
 
 // Componente principal da aplicação autenticada
 function AuthenticatedApp() {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
   const { user } = useAuth()
 
   return (
     <div className="flex h-screen bg-gray-100">
       {/* Sidebar */}
-      <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+      <Sidebar />
       
       {/* Main Content */}
-      <div className="flex-1 flex flex-col overflow-hidden">
+      <div className="flex-1 flex flex-col overflow-hidden lg:ml-0">
         {/* Header */}
-        <Header onMenuClick={() => setSidebarOpen(true)} user={user} />
+        <Header user={user} />
         
         {/* Page Content */}
-        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100">
+        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 p-4 lg:p-6">
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/dashboard" element={<Dashboard />} />
